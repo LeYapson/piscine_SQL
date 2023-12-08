557f72ff2edc60ae523c6ce16ca2d4cba8e81cf8
théau Yapi <theau.yapi@ynov.com>
2023-12-08 14:53:43 +0100

    streamdeck upload

--- join-customers-invoices.sql
+++ join-customers-invoices.sql
@@ -1,12 +1,9 @@
--- Left join between customers and invoices tables to list customer information and invoice total prices
 SELECT
     customers.FirstName,
     customers.LastName,
     customers.Company,
-    COALESCE(SUM(invoices.Total), 0) AS InvoiceTotalPrice
+    invoices.Total AS InvoiceTotalPrice
 FROM
     customers
-LEFT JOIN
-    invoices ON customers.CustomerId = invoices.CustomerId
-GROUP BY
-    customers.CustomerId;
+JOIN
+    invoices ON customers.CustomerId = invoices.CustomerId;
