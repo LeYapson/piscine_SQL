5ef5ce4c0a66f8e02e907151d079a3ed9fb90471
théau Yapi <theau.yapi@ynov.com>
2023-12-08 14:14:47 +0100

    streamdeck upload

--- select-customer-where.sql
+++ select-customer-where.sql
@@ -1,4 +1,4 @@
 -- Retrieve customer data for the company 'Microsoft Corporation'
 SELECT *
     FROM customers
-    WHERE Company = 'Microsoft Corporation';
+    WHERE Company = 'Microsoft Corporation'
