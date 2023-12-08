aa4d284e54bc0558cb883067e9e3116256d02b5a
théau Yapi <theau.yapi@ynov.com>
2023-12-08 15:06:26 +0100

    streamdeck upload

--- left-join.sql
+++ left-join.sql
@@ -1,7 +1,8 @@
 SELECT
     artists.Name,
-    albums.Title AS 'AlbumTitle'
+    albums.Title AS AlbumTitle
 FROM
     artists
-LEFT JOIN albums ON artists.ArtistId = albums.AlbumId
+LEFT JOIN
+    albums ON artists.ArtistId = albums.ArtistId
 LIMIT 100;
