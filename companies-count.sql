71c529001cb17284d522d6dce5768a789e8b6581
théau Yapi <theau.yapi@ynov.com>
2023-12-18 17:40:40 +0100

    efeifeif

--- companies-count.sql
+++ companies-count.sql
@@ -1,7 +1,4 @@
 -- Count the number of customers who have a Company
-SELECT
-    COUNT(*) NbCompanies
-FROM
-    customers
-WHERE
-    Company IS NOT NULL;
+SELECT COUNT(*) NbCompanies
+FROM customers
+WHERE Company IS NOT NULL
