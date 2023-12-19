56ea0c4b66bb7e694257303abbed6b3fda986bad
théau Yapi <theau.yapi@ynov.com>
2023-12-19 15:02:39 +0100

    yes

--- genre-tracks-average.sql
+++ genre-tracks-average.sql
@@ -2,4 +2,4 @@ SELECT genres.Name, avg(Milliseconds) AverageDuration
 FROM genres
 JOIN tracks ON genres.GenreId = tracks.GenreId
 GROUP BY genres.Name, tracks.GenreId
-ORDER BY AverageDuration DESC 
+ORDER BY AverageDuration DESC
