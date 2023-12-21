361689a7c7790f2d2f232e3e9b111786b1a8f31a
théau Yapi <theau.yapi@ynov.com>
2023-12-21 14:35:23 +0100

    ifndinfdinvnide

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -1,9 +1,9 @@
 SELECT
     InvoiceId,
-    TotalAmount,
+    Total,
     CASE
-        WHEN TotalAmount < 5 THEN 'Price lower than 5$'
-        WHEN TotalAmount < 10 THEN 'Price lower than 10$'
+        WHEN Total < 5 THEN 'Price lower than 5$'
+        WHEN Total < 10 THEN 'Price lower than 10$'
         ELSE 'Price greater than 10$'
     END AS PriceCategory
 FROM
