4283ca5ae310bc9d46d080f9d325ffa8a329083f
théau Yapi <theau.yapi@ynov.com>
2023-12-21 14:48:56 +0100

    ifndinfdinvnide

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -5,7 +5,8 @@ SELECT
         WHEN Total < 5 THEN 'Price lower than 5$'
         WHEN Total < 10 THEN 'Price lower than 10$'
         ELSE 'Price greater than 10$'
-    END AS PriceCategory
+    END
+AS 'CASE'
 FROM
     invoices
 LIMIT 100;
