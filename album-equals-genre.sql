e9a6a55ac80a1260604c1d1acf52f6688dcf2bb9
théau Yapi <theau.yapi@ynov.com>
2023-12-26 19:27:46 +0100

    albumEqualGenre

--- album-equals-genre.sql
+++ album-equals-genre.sql
@@ -1,6 +1,5 @@
-SELECT
-    *
-FROM
-    tracks
-WHERE
-    GenreId IN (SELECT GenreId FROM genres WHERE GenreName = tracks.Name);
+SELECT albums.AlbumId, artists.ArtistId, albums.Title FROM albums
+	INNER JOIN tracks ON tracks.AlbumId = albums.AlbumId
+	INNER JOIN artists ON artists.ArtistId = albums.ArtistId
+    WHERE albums.Title IN (SELECT genres.Name FROM genres)
+    GROUP BY albums.AlbumId;
