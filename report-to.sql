58fc1ac0c882f89524731cf844956d731af19c71
théau Yapi <theau.yapi@ynov.com>
2024-01-09 16:05:03 +0100

    derniere quete sql

---
+++ report-to.sql
@@ -0,0 +1,7 @@
+SELECT
+    e.EmployeeId,
+    e.FirstName || ' ' || e.LastName AS EmployeeName,
+    m.FirstName || ' ' || m.LastName AS ReportsTo
+FROM employees e
+LEFT JOIN
+    employees m ON e.ReportsTo = m.EmployeeId;
