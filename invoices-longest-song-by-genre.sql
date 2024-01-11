090c1b46279765f848fd77c46efd1d26c9045a6a
théau Yapi <theau.yapi@ynov.com>
2024-01-11 17:38:13 +0100

    isdfhifh

--- invoices-longest-song-by-genre.sql
+++ invoices-longest-song-by-genre.sql
@@ -1,12 +1,9 @@
-SELECT DISTINCT
-    i.InvoiceId
+SELECT DISTINCT i.InvoiceId
 FROM invoices i
-JOIN invoice_items it ON i.InvoiceId = it.invoiceId
-WHERE it.TrackId IN(
-SELECT
-    t.TrackId
-FROM songs t
-JOIN genres g ON t.GenreId = g.GenreId
-GROUP BY g.GenreId
-HAVING max(t.Milliseconds))
+JOIN invoice_items it ON i.InvoiceId = it.InvoiceId
+WHERE it.TrackId IN (SELECT  t.TrackId
+                    FROM tracks t
+                    JOIN genres g ON t.GenreId = g.GenreId
+                    GROUP BY g.GenreId
+                    HAVING max(t.Milliseconds))
 ORDER BY i.InvoiceId;
