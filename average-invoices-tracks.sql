783e41225aeb04a1c61e2befe7273ab828be3a81
théau Yapi <theau.yapi@ynov.com>
2024-01-12 14:26:46 +0100

    iofndin

--- average-invoices-tracks.sql
+++ average-invoices-tracks.sql
@@ -1,28 +1,5 @@
-SELECT
-    i.InvoiceId
-    AVG(invoice_items.UnitPrice) AS AverageCostBySongs,
-    SUM(tracks.Milliseconds) AS TotalLengthOfSongs,
-    SUM(invoice_items.UnitPrice) / (SUM(tracks.Milliseconds) / 1000.0)
-FROM
-    invoices i
-JOIN invoice_items ON i.InvoiceId = invoice_items.InvoiceId
-JOIN (
-    SELECT
-        tracks.TrackId,
-        tracks.Milliseconds
-    FROM
-        tracks
-    JOIN
-        genres ON tracks.GenreId = genres.GenreId
-    JOIN (
-        SELECT
-            tracks.GenreId,
-            RANK() OVER (PARTITION BY genres.GenreId ORDER BY tracks.Milliseconds DESC) AS SongRank
-        FROM
-            tracks
-        JOIN
-            genres ON tracks.GenreId = genres.GenreId
-    ) ranked_songs ON tracks.GenreId = ranked_songs.GenreId AND ranked_songs.SongRank = 1
-) longest_songs ON invoice_items.TrackId = longest_songs.TrackId
-GROUP BY
-    invoices.InvoiceId;
+SELECT invoices.InvoiceId, ROUND(SUM(invoice_items.unitprice) / COUNT(*), 2) AS "Average Price", SUM(tracks.Milliseconds)/1000 AS "Track Total Time", ROUND((SUM(invoice_items.UnitPrice) / COUNT(invoice_items.TrackId)) / (SUM(tracks.Milliseconds) / 1000), 5) || '€' AS "Price by second"
+FROM invoices
+INNER JOIN invoice_items ON invoice_items.invoiceid = invoices.invoiceid
+INNER JOIN tracks ON tracks.TrackId = invoice_items.trackid
+GROUP BY invoices.invoiceid
